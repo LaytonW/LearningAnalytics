8d53717f858beaef2d6592dc0ec2a460b2a05a30
972961854@qq.com <972961854@qq.com>
2016-12-05 18:18:06 +0800

    add fake data at presenation/url.py

--- presentation/urls.py
+++ presentation/urls.py
@@ -1,5 +1,26 @@
 from django.conf.urls import include, url
 import presentation.views as views
+from .models import Instructor, Student, Course
+from django.contrib.auth.models import User
+
+
+def init():
+    try:
+        user = User.objects.create_user(username='test_2', password='1234qwer')
+        user_1 =  User.objects.create_user(username='test_3', password='1234qwer')
+        instructor = Instructor.objects.create(user=user)
+        instructor_1 = Instructor.objects.create(user=user_1)
+        course = Course.objects.create(name='test', instructor=instructor,average=10)
+        course_1 = Course.objects.create(name='test_1', instructor=instructor,average=15)
+        course_2 = Course.objects.create(name='test_2', instructor=instructor,average=13)
+        student = Student.objects.create(enrolledCourse=course, risk = 0.2, grades = [80,80,80])
+        student_1 = Student.objects.create(enrolledCourse=course, risk = 1, grades = [30,40,50])
+        student_2 = Student.objects.create(enrolledCourse=course, risk = 0.5, grades = [40,40,50])
+    except:
+        pass
+
+
+init()
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
